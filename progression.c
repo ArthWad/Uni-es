0191ab59b48fdb3f7e46c9f309d206812d1b6bee
ArthWad <150542972+ArthWad@users.noreply.github.com>
2023-11-11 18:10:33 +0100

    Create progression.c

---
+++ progression.c
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+using namespace std;
+
+//var
+double i, d, n, f, g;
+
+
+
+//if
+void prog(double i) {
+    g = (i - n);
+if(g == (f - d)){
+cout<<"The progression number is: " <<g<< endl;
+} else {
+    cout<<"There's no progression" << endl;
+}
+}
+
+//prog
+
+int main(){
+cout <<"CHOOSE NUMBERS"<< "\n";
+while(cin >> n >> i >> d >> f){
+    prog(i);
+return 0;
+}
+}
